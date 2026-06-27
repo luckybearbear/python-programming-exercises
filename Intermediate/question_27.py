11ea2efbc58fee5793d5370a782168c2a04226cf
luckybearbear <jun.zhang@pexetech.com>
2026-06-27 10:24:51 +0800

    question 27、28

--- Intermediate/question_27.py
+++ Intermediate/question_27.py
@@ -3,3 +3,9 @@
 
 提示：使用 str() 函数将整数转换为字符串。
 """
+
+def int2Str(num: int) -> str:
+    return str(num)
+
+if __name__ == "__main__":
+    print(int2Str(12))
--- Intermediate/question_28.py
+++ Intermediate/question_28.py
@@ -3,3 +3,17 @@
 
 提示：使用 int() 将字符串转换为整数。
 """
+
+
+def add_str_num(num1_str, num2_str):
+    # 将字符串转为整数
+    num1 = int(num1_str)
+    num2 = int(num2_str)
+    # 计算和
+    total = num1 + num2
+    # 控制台打印结果
+    print(total)
+
+# 测试示例
+add_str_num("123", "456")
+add_str_num("-10", "20")
